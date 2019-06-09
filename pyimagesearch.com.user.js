416ceed651a5c0a74ea9f1898209dba6aad465a1
JCBrown602 <3117301+JCBrown602@users.noreply.github.com>
2019-06-09 14:16:13 -0700

    Finally got changed over to repo from gist. Mainly just for practice, but the image was a bit bothersome too.

--- pyimagesearch.com.user.js
+++ pyimagesearch.com.user.js
@@ -1,22 +1,25 @@
 // ==UserScript==
-// @name nocarousel on pyimagesearch.com (Opera) | Jason
-// @namespace https://userscripts.org/scripts/show/######
-// @description Turns off the annoying sidebar carousel. Sorry Adrian. The script finds a <div> with the ID of "pyimg_gurus_carousel" and assigns it a new ID of "nocarousel", thus preventing the carousel function. Script adds innerHTML as a positive visual.
+// @name nocarousel for pyimagesearch.com (Opera/Chrome) | Jason
+// @description Turns off the annoying sidebar carousel. Sorry Adrian. The script finds a <div> with the ID of "pyimg_gurus_carousel" and assigns it a new ID of "nocarousel", thus preventing the carousel function. Script adds innerHTML as a positive visual indication that it's working. ( and it's my first userscript! - hence, more meta than meat! )
+// @namespace https://greasyfork.org/
+// @version 2.0
+// @match https://www.pyimagesearch.com/*
+// @updateURL 	https://github.com/JCBrown602/pyimagesearch.com/raw/master/pyimagesearch.com.user.js
+// @downloadURL https://github.com/JCBrown602/pyimagesearch.com.git
+// @license MIT
+// @supportURL https://github.com/JCBrown602/pyimagesearch.com/issues
+// @compatible opera
+// @compatible chrome
 // @author Jason | jetcanlabs.com
-// @version 1.0
 // @encoding utf-8
 // @grant none
-// @include https://www.pyimagesearch.com/*
-// @license https://creativecommons.org/licenses/by-sa/4.0/
-// @icon https://gist.githubusercontent.com/JCBrown602/35733862f9b949614a69f42e2ef813f1/raw/f84b755a8f16310cde99d679fcdef781d2b64435/cat.ico
-// @homepage https://github.com/jcbrown602/pyimagesearch.com.user.js/
-// @updateURL https://raw.githubusercontent.com/JCBrown602/nocarousel//pyimagesearch.com.user.js
-// @downloadURL https://gist.github.com/JCBrown602/35733862f9b949614a69f42e2ef813f1/raw/9a36914303577cfd3c72f1b91615a3f689e7cfb4/pyimagesearch.com.user.js
+// @icon https://raw.githubusercontent.com/JCBrown602/pyimagesearch.com/master/cat.ico
+// @homepage https://github.com/JCBrown602/pyimagesearch.com
 // ==/UserScript==
 
 // Browser developer tools "inspect element" gives ID of offending <div>
   var query = "pyimg_gurus_carousel";
   var element = document.getElementById(query);
 
-  document.getElementById(query).id = "nocarousel";
-  document.getElementById("nocarousel").innerHTML = "nocarousel: carousel stopped by monkeys.";
+  element.id = "nocarousel";
+  element.innerHTML = "nocarousel: carousel stopped by monkeys.";
